330d0162bba3947379a360942aee6c8773195efe
Isax82 <151661172+Isax820@users.noreply.github.com>
2026-02-26 16:10:45 +0100

    Auth file

---
+++ auth.js
@@ -0,0 +1,44 @@
+function register() {
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if(!username || !password){
+alert("Remplis tous les champs !");
+return;
+}
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+if(users.find(u => u.username === username)){
+alert("Utilisateur déjà existant !");
+return;
+}
+
+users.push({ username, password });
+
+localStorage.setItem("users", JSON.stringify(users));
+
+alert("Compte créé !");
+}
+
+function login() {
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+const user = users.find(
+u => u.username === username && u.password === password
+);
+
+if(!user){
+alert("Identifiants incorrects !");
+return;
+}
+
+localStorage.setItem("loggedUser", username);
+
+window.location.href = "index.html";
+}
