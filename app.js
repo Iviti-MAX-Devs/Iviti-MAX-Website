be97ef2e30af7195334447f417d89eab9c0da8de
Isax82 <151661172+Isax820@users.noreply.github.com>
2026-02-26 16:10:14 +0100

    JS file

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
+// Protection page
+const user = localStorage.getItem("loggedUser");
+
+if (!user) {
+window.location.href = "login.html";
+}
+
 let videos = [];
 
 fetch("videos.json")
-  .then(res => res.json())
-  .then(data => {
-    videos = data;
-    displayVideos(videos);
-  });
+.then(res => res.json())
+.then(data => {
+videos = data;
+displayVideos(videos);
+});
 
 function displayVideos(list) {
-  const container = document.getElementById("videosContainer");
-  container.innerHTML = "";
+const container = document.getElementById("videosContainer");
+container.innerHTML = "";
 
-  list.forEach(v => {
-    const card = document.createElement("div");
-    card.className = "video-card";
+list.forEach(v => {
 
-    card.innerHTML = `
-      <img src="${v.thumbnail}" alt="${v.title}">
-      <h3>${v.title}</h3>
-      <button onclick="play('${v.url}')">▶ Regarder</button>
-    `;
+const card = document.createElement("div");
+card.className = "video-card";
 
-    container.appendChild(card);
-  });
+card.innerHTML = `
+<img src="${v.thumbnail}">
+<h3>${v.title}</h3>
+<button onclick="play('${v.url}')">▶ Regarder</button>
+`;
+
+container.appendChild(card);
+});
 }
 
 function filterBy(category) {
-  if (category === "All") {
-    displayVideos(videos);
-  } else {
-    displayVideos(videos.filter(v => v.category === category));
-  }
+if(category === "All") {
+displayVideos(videos);
+} else {
+displayVideos(videos.filter(v => v.category === category));
+}
 }
 
 function play(url) {
-  window.open(url, "_blank");
+window.open(url, "_blank");
 }
 
-document.getElementById("searchInput").oninput = function () {
-  const term = this.value.toLowerCase();
-  displayVideos(
-    videos.filter(v =>
-      v.title.toLowerCase().includes(term)
-    )
-  );
+document.getElementById("searchInput").oninput = function() {
+const term = this.value.toLowerCase();
+
+displayVideos(
+videos.filter(v =>
+v.title.toLowerCase().includes(term)
+)
+);
 };
+
+function logout() {
+localStorage.removeItem("loggedUser");
+window.location.href = "login.html";
+}
